45e75d3ecbfa235f4ae263b1fd78af7718a666c2
iivkis <razmolodinivan@mail.ru>
2025-09-29 00:41:25 +0700

    init web

---
+++ rukkola-web/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import {Button} from "$lib/components/ui/button"
+
+    let count = $state(0)
+
+    let inc = () => {
+        count++
+    }
+</script>
+
+
+<div>Count: <span>{count}</span></div>
+<Button onclick={inc}>Inc</Button>
